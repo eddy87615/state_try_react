7ce0b6865068391c2005fb69ce122bfc7f080d68
Eddy Chen <eddychen615@gmail.com>
2023-10-23 22:30:28 +0900

    children props

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -41,9 +41,7 @@ function Steps() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClock={handlePrevious}>
@@ -55,6 +53,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
